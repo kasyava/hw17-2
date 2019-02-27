237ffa72db4cdcf29da2ae7690212232d0db98d3
kasyava <budo_slava@mail.ru>
2019-02-27 18:50:38 +0600

    добавил компонент для Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
 import Menu from './components/Menu/Menu'
+import Footer from './components/Footer/Footer'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Menu />
+        <Footer />
       </div>
     );
   }
