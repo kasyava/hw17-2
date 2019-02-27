b56bab8a9cd0c6546fe6fcc835d58b123459231a
kasyava <budo_slava@mail.ru>
2019-02-27 18:45:44 +0600

    добавил компонент для Меню

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
+import Menu from './components/Menu/Menu'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Menu />
       </div>
     );
   }
